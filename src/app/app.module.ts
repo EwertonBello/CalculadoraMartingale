7edb9d949dbad80187067231e454b3f2f0648783
EwertonBello <ewerton.bello1212@gmail.com>
2019-09-13 18:07:11 -0300

    Atualizando projeto em branco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [
-    StatusBar,
-    SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+	public rendimento1: number = 0;
+	public perda1: number = 0;
+	public adicional1: number = 0;	
+	public resultado1;	
 
+	public banca: number = 0;
+	public rendimento2: number = 0;
+	public lucro: number = 0;
+	public resultado2;	
+
+	constructor() {}
+
+	calcularSobrePerda() 
+	{
+		let resultado: number = (this.perda1+this.adicional1)/(this.rendimento1/100);
+		this.resultado1 = resultado
+	}  
+
+	calcularMartingales() 
+	{
+    let valor_inicial = (this.lucro)/(this.rendimento2/100);
+
+    let cont: number = 1
+    let total_martingale = valor_inicial
+    let niveis: string = "Investimento "+cont+": "+valor_inicial
+	    while(this.banca >= total_martingale) 
+	    {  	
+	    	cont += 1
+	    	let nivel_atual = ((total_martingale+this.lucro)/(this.rendimento2/100))
+	        total_martingale += nivel_atual
+	        if (total_martingale <= this.banca)
+	            niveis += "\nInvestimento "+cont+": "+nivel_atual
+	    }
+
+	    this.resultado2 = niveis
+	}
 }
